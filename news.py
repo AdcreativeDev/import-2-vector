36528662ad4528d327aa372f987109cf2662b35b
AdcreativeDev <139262376+AdcreativeDev@users.noreply.github.com>
2023-07-12 22:42:33 +0800

    Add files via upload

--- news.py
+++ news.py
@@ -9,10 +9,20 @@ from langchain.chains import SequentialChain
 from PIL import Image
 from langchain.document_loaders import WebBaseLoader
 import requests
+from PIL import Image
 import datetime
 
+# requirements.txt
+# streamlit
+# langchain
+# requests
+# DateTime
+# openai
+# newsapi
+# bs4
+
 st.set_page_config(
-    page_title="Demo Page",
+    page_title="Text -> Vector",
     page_icon="🧊",
     layout="wide",
     initial_sidebar_state="expanded",
@@ -24,96 +34,101 @@ hide_menu_style = """
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
+image = Image.open("ironman-banner.jpg")
+st.image(image, caption='created by MJ')
+
 
 
 url = 'https://www.scmp.com/news/hong-kong/transport/article/3227297/hongkongers-flood-hk-express-website-carrier-gives-away-21626-free-tickets-many-fail-enter-booking'
 
 # Get the current datetime as a string
 now = datetime.datetime.now()
-filename = now.strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
-
-
+Extractionfilename = now.strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
 sample_text = """
 """
-news_endpoint = 'https://newsapi.org/v2/top-headlines?country=HK&apiKey=3302aaafa5ae4d9eb441f833e249ce77'
+
+
+
+def read_file(filename):
+    try:
+        with open(filename, "r") as file:
+            content = file.read()
+        return content
+    except FileNotFoundError:
+        print(f"Error: File '{filename}' not found.")
+    except:
+        print(f"Error: Could not read file '{filename}'.")
+
+
+def extraction():
+    response = requests.get(news_endpoint)
+
+    AllWebLinks = ""
+    if response.status_code == 200:
+        data = response.json()
+
+
+        st.caption('Start capture web content:')
+        articles = data['articles']
+        st.caption(f'Total {len(articles)} web page(s) will  extract')
+        NoOfProcess = 1
+        with open(Extractionfilename, "w") as file:
+            for article in articles:
+
+                title = article['title'] 
+                url = article['url']
+
+                webLink = "\n<a href='"  +  article['url']    + "'>" +  article['title']  + "</>"
+                AllWebLinks = AllWebLinks + webLink
+                
+                # get the content of the artice
+                loader = WebBaseLoader([url])
+                data = loader.load()
+                WebContent = data[0].page_content.replace('\n', '')
+
+                if len(WebContent) > 0:
+                    WebContent_brief = WebContent[:100]
+                    file.write("\n\n")
+                    file.write("[TITLE]\n" + title + "\n")
+                    file.write("[CONTENT]\n" + WebContent + "\n")
+                    st.caption(f' ▶️   processing : ' + str(NoOfProcess) + ' file ' + url) 
+                    st.caption(f' ✔️ completed : {WebContent_brief}  ....' )
+                    NoOfProcess = NoOfProcess + 1
+            if (NoOfProcess > 0):
+                st.caption(f'✔️ Extraction completed, total : {str(NoOfProcess - 1)} website.')
+                col1 , col2  = st.columns(2)
+                with col1:  
+                    with st.expander(" Json Format:1"):
+                        st.code(data)
+                with col2:
+                    with st.expander(" Json Format:2"):
+                        st.write(data)
+
+
+    else:
+        print('❌❌❌  New API cannot fetch any content')
+
+    with st.expander("Links contained in responses"):
+        st.code(AllWebLinks)
+
+    file.close()
+
+
+
+def DumpFileContents():
+    file_content = read_file(filename=Extractionfilename)
+    if file_content is not None:
+        with st.expander(f'{Extractionfilename} content'):
+            st.code(file_content)
+    else:
+        print("Error: Could not read file.")
+
+
 news_endpoint = 'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=3302aaafa5ae4d9eb441f833e249ce77'
-st.subheader(f':blue[Extract content from news api endpoint]   {news_endpoint}')
-
-response = requests.get(news_endpoint)
-st.subheader('response text ')
-# st.write(response.text)
-output = ""
-if response.status_code == 200:
-    data = response.json()
-    with st.expander(" Json 1"):
-        st.code(data)
-    with st.expander(" Json 2"):
-        st.write(data)
-    st.subheader('Json content')
-    articles = data['articles']
+st.subheader(f'🌎 :blue[News API Extract]')
+st.caption(f'endpoint : {news_endpoint}')
+st.caption('Press below Button to start extraction')
+if st.button('start', type='primary'):
+    extraction()
+    DumpFileContents()
     
-    with open(filename, "w") as file:
-        for article in articles:
-
-            title = article['title'] 
-            url = article['url']
-            webLink = "\n<a href='"  +  article['url']    + "'>" +  article['title']  + "</>"
-            output = output + webLink
-            st.info(webLink)
-            
-            # get the content of the artice
-            loader = WebBaseLoader([url])
-            data = loader.load()
-            WebContent = data[0].page_content.replace('\n', '')
-            file.write("\n\n")
-            file.write("[TITLE]\n" + title + "\n")
-            file.write("[CONTENT]\n" + WebContent + "\n")
-            st.info('Writing 1 LINE')
-            st.warning(WebContent)
-
-else:
-    print('Error : could not get articles')
-
-with st.expander("Article"):
-    st.code(output)
-
-file.close()
-
-# # Init
-# newsapi = NewsApiClient(api_key='3302aaafa5ae4d9eb441f833e249ce77')
-
-# # /v2/top-headlines
-# top_headlines = newsapi.get_top_headlines(q='bitcoin',
-#                                           sources='bbc-news,the-verge',
-#                                           language='en')
-
-
-# /v2/everything
-# all_articles = newsapi.get_everything(q='bitcoin',
-#                                       sources='bbc-news,the-verge',
-#                                       domains='bbc.co.uk,techcrunch.com',
-#                                       from_param='2017-12-01',
-#                                       to='2017-12-12',
-#                                       language='en',
-#                                       sort_by='relevancy',
-#                                       page=2)
-
-# /v2/top-headlines/sources
-# sources = newsapi.get_sources()
-
-
-
-# loader = WebBaseLoader([url])
-# data = loader.load()
-# content = data[0].page_content.replace('\n', '')
-# st.info(content)
-
-# get the content of the artice
-# url = 'https://news.google.com/rss/articles/CCAiC3VUSExLWGxTRnFnmAEB?oc=5'
-# loader = WebBaseLoader([url])
-# data = loader.load()
-# WebContent = data[0].page_content.replace('\n', '')
-# st.write(WebContent)
-
-
-
